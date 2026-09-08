5dcb81259b8f8067a26112b71baf3b18f1526f9e
Ahmed <axmedkafi11@gmail.com>
2026-09-08 10:38:17 +0300

    feat: add HTTP server and student routes

--- main.go
+++ main.go
@@ -1,35 +1,87 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 type Student struct {
-	ID   int
-	Name string
-	Age  int
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func students(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+
+	case http.MethodGet:
+		student := Student{
+			ID:   1001,
+			Name: "Muscab",
+			Age:  25,
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(student)
+
+	case http.MethodPost:
+		var student Student
+
+		err := json.NewDecoder(r.Body).Decode(&student)
+
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintln(w, "Invalid JSON")
+			return
+		}
+
+		w.WriteHeader(http.StatusCreated)
+		fmt.Fprintln(w, "Student Created:", student.Name)
+
+	case http.MethodPut:
+		var student Student
+
+		err := json.NewDecoder(r.Body).Decode(&student)
+
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintln(w, "Invalid JSON")
+			return
+		}
+
+		fmt.Fprintln(w, "Student Updated:", student.Name)
+
+	case http.MethodDelete:
+		fmt.Fprintln(w, "Student Deleted")
+
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintln(w, "Method Not Allowed")
+	}
 }
 
 func main() {
-	ch := make(chan Student)
+	http.HandleFunc("/students", students)
 
-	go func() {
-		ch <- Student{
-			ID:   1001,
-			Name: "Muscab",
-			Age:  20,
-		}
-	}()
+	fmt.Println("Server running on http://localhost:8080")
 
-	go func() {
-		ch <- Student{
-			ID:   2002,
-			Name: "Ali",
-			Age:  43,
-		}
-	}()
-
-	students1 := <-ch
-	students2 := <-ch
-
-	fmt.Println("Name 1:", students1.Name)
-	fmt.Println("Name 2:", students2.Name)
+	http.ListenAndServe(":8080", nil)
 }
+
+
+/*
+Another powershell
+curl.exe http://localhost:8080/students
+
+then 
+$body = '{"name":"Ahmed","age":25}'
+Invoke-RestMethod -Uri "http://localhost:8080/students" -Method POST -ContentType "application/json" -Body $body
+
+$body = '{"name":"Muscab Ahmed","age":21}'
+Invoke-RestMethod -Uri "http://localhost:8080/students" -Method PUT -ContentType "application/json" -Body $body
+
+curl.exe -X DELETE http://localhost:8080/students
+
+*/
